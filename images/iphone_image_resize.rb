8b45b7d1688bc3f20fd61a4a70b8e026fb8e307e
Warut Surapat <s.warut@gmail.com>
2013-06-11 16:42:58 +0700

    update image resizer

--- images/iphone_image_resize.rb
+++ images/iphone_image_resize.rb
@@ -13,7 +13,10 @@ def extract_argrument(args0, args1)
   matched_target = args0.match(/--target=/) ?
     args0.extract_file_for(:target) :
     args1.extract_file_for(:target)
-  return [matched_source, matched_target]
+
+  return [
+    matched_source.gsub(/\/$/, ""),
+    matched_target.gsub(/\/$/, "")]
 end
 
 if ARGV.size == 2
@@ -24,17 +27,52 @@ if ARGV.size == 2
     puts "Target: #{target}"
 
     source_dir = Dir.new(source)
-    target_dir = Dir.new(target)
+    # target_dir = Dir.new(target)
+    ios_dir = "#{target}/ios"
+    android_xhdpi = "#{target}/android/xhdpi"
+    android_hdpi  = "#{target}/android/hdpi"
+    android_mdpi  = "#{target}/android/mdpi"
 
-    source_dir.each do |file|
-      if file != "." && file != ".." && file != ".DS_Store"
-        target_file_name = file.gsub(/@2x/, "")
-        image = Magick::Image::read("#{source}/#{file}").first
-        scaled_image = image.scale(0.5)
-        scaled_image.write "#{target}/#{target_file_name}"
-        puts "successfully save #{target}/#{target_file_name}"
-      end
+    Dir.mkdir(ios_dir) unless Dir.exist?(ios_dir)
+    unless Dir.exist?("#{target}/android")
+      Dir.mkdir("#{target}/android")
+      Dir.mkdir(android_xhdpi)
+      Dir.mkdir(android_hdpi)
+      Dir.mkdir(android_mdpi)
     end
+
+    ignored_files = %w{. .. .DS_Store android ios}
+    puts "--------"
+    files = (source_dir.to_a - ignored_files)
+    files = files.reject{ |f| File.directory?(f) }
+    puts files
+    puts "--------"
+    (source_dir.to_a - ignored_files).each do |file|
+
+      target_file_name = file
+      target_file_name2x = "#{File.basename(file, ".*")}@2x#{File.extname(file)}"
+      puts "read " + "#{source}/#{file}"
+      image = Magick::Image::read("#{source}/#{file}").first
+
+      # ios
+      ios_2x_scaled_image = image.scale(0.888)
+      puts "writing #{ios_dir}/#{target_file_name2x}"
+      ios_2x_scaled_image.write "#{ios_dir}/#{target_file_name2x}"
+      ios_scaled_image = image.scale(0.444)
+      puts "writing #{ios_dir}/#{target_file_name}"
+      ios_scaled_image.write "#{ios_dir}/#{target_file_name}"
+
+      # android
+      android_mdpi_scaled_image = image.scale(0.444)
+      puts "writing #{android_mdpi}/#{target_file_name}"
+      android_mdpi_scaled_image.write "#{android_mdpi}/#{target_file_name}"
+      android_hdpi_scaled_image = image.scale(0.6666)
+      puts "writing #{android_hdpi}/#{target_file_name}"
+      android_hdpi_scaled_image.write "#{android_hdpi}/#{target_file_name}"
+      puts "writing #{android_xhdpi}/#{target_file_name}"
+      image.write "#{android_xhdpi}/#{target_file_name}"
+    end
+
     puts "Done!."
   else
     puts "Please make sure that both source and target are valid folders."
