d16322e2e8dca0a60e9b0856964a6518701e575f
Warut Surapat <s.warut@gmail.com>
2013-06-28 11:50:49 +0700

    Enchane how image resizer work. Now support android and ios specific image. If you wanted to public only android or ios specific image, just name the file  with xxx_android_xxx or xxx_ios_xxx.

--- images/mobile_image_resizer.rb
+++ images/mobile_image_resizer.rb
@@ -4,6 +4,14 @@ class String
   def extract_file_for(key)
     self.gsub("--#{key.to_s}=", "")
   end
+
+  def android_specific?
+    self.match(/_android_/)
+  end
+
+  def ios_specific?
+    self.match(/_ios_/)
+  end
 end
 
 def extract_argrument(args0, args1)
@@ -19,6 +27,48 @@ def extract_argrument(args0, args1)
     matched_target.gsub(/\/$/, "")]
 end
 
+def save_for_ios(image, ios_dir, target_file_name)
+
+  extension = File.extname(target_file_name)
+  target_file_name2x =
+    "#{File.basename(target_file_name, ".*")}@2x#{extension}"
+
+  # ios
+  ios_2x_scaled_image = image.scale(0.888)
+  puts "writing #{ios_dir}/#{target_file_name2x}"
+  ios_2x_scaled_image.write "#{ios_dir}/#{target_file_name2x}"
+  ios_scaled_image = image.scale(0.444)
+  puts "writing #{ios_dir}/#{target_file_name}"
+  ios_scaled_image.write "#{ios_dir}/#{target_file_name}"
+end
+
+def save_for_android(image, android_dir, target_file_name)
+  android_xhdpi = "#{android_dir}/xhdpi"
+  android_hdpi  = "#{android_dir}/hdpi"
+  android_mdpi  = "#{android_dir}/mdpi"
+  android_ldpi  = "#{android_dir}/ldpi"
+
+  # android
+  # mdpi
+  android_mdpi_scaled_image = image.scale(0.444)
+  puts "writing #{android_mdpi}/#{target_file_name}"
+  android_mdpi_scaled_image.write "#{android_mdpi}/#{target_file_name}"
+
+  # ldpi
+  android_ldpi_scaled_image = android_mdpi_scaled_image.scale(0.5)
+  puts "writing #{android_ldpi}/#{target_file_name}"
+  android_ldpi_scaled_image.write "#{android_ldpi}/#{target_file_name}"
+
+  # hdpi
+  android_hdpi_scaled_image = image.scale(0.6666)
+  puts "writing #{android_hdpi}/#{target_file_name}"
+  android_hdpi_scaled_image.write "#{android_hdpi}/#{target_file_name}"
+
+  # xhdpi
+  puts "writing #{android_xhdpi}/#{target_file_name}"
+  image.write "#{android_xhdpi}/#{target_file_name}"
+end
+
 if ARGV.size == 2
   source, target = extract_argrument(ARGV[0], ARGV[1])
   if File.directory?(source) && File.directory?(target)
@@ -28,16 +78,15 @@ if ARGV.size == 2
 
     source_dir = Dir.new(source)
     ios_dir = "#{target}/ios"
-    android_xhdpi = "#{target}/android/xhdpi"
-    android_hdpi  = "#{target}/android/hdpi"
-    android_mdpi  = "#{target}/android/mdpi"
+    android_dir = "#{target}/android"
 
     Dir.mkdir(ios_dir) unless Dir.exist?(ios_dir)
     unless Dir.exist?("#{target}/android")
-      Dir.mkdir("#{target}/android")
-      Dir.mkdir(android_xhdpi)
-      Dir.mkdir(android_hdpi)
-      Dir.mkdir(android_mdpi)
+      Dir.mkdir(android_dir)
+      Dir.mkdir("#{android_dir}/xhdpi")
+      Dir.mkdir("#{android_dir}/hdpi")
+      Dir.mkdir("#{android_dir}/mdpi")
+      Dir.mkdir("#{android_dir}/ldpi")
     end
 
     ignored_files = %w{. .. .DS_Store android ios}
@@ -45,30 +94,21 @@ if ARGV.size == 2
     (source_dir.to_a - ignored_files).each do |file|
 
       target_file_name = file
-      target_file_name2x = "#{File.basename(file, ".*")}@2x#{File.extname(file)}"
       puts "read " + "#{source}/#{file}"
       image = Magick::Image::read("#{source}/#{file}").first
 
-      # ios
-      ios_2x_scaled_image = image.scale(0.888)
-      puts "writing #{ios_dir}/#{target_file_name2x}"
-      ios_2x_scaled_image.write "#{ios_dir}/#{target_file_name2x}"
-      ios_scaled_image = image.scale(0.444)
-      puts "writing #{ios_dir}/#{target_file_name}"
-      ios_scaled_image.write "#{ios_dir}/#{target_file_name}"
+      if target_file_name.android_specific?
+        save_for_android(image, android_dir, target_file_name)
+      elsif target_file_name.ios_specific?
+        save_for_ios(image, ios_dir, target_file_name)
+      else
+        save_for_android(image, android_dir, target_file_name)
+        save_for_ios(image, ios_dir, target_file_name)
+      end
 
-      # android
-      android_mdpi_scaled_image = image.scale(0.444)
-      puts "writing #{android_mdpi}/#{target_file_name}"
-      android_mdpi_scaled_image.write "#{android_mdpi}/#{target_file_name}"
-      android_hdpi_scaled_image = image.scale(0.6666)
-      puts "writing #{android_hdpi}/#{target_file_name}"
-      android_hdpi_scaled_image.write "#{android_hdpi}/#{target_file_name}"
-      puts "writing #{android_xhdpi}/#{target_file_name}"
-      image.write "#{android_xhdpi}/#{target_file_name}"
     end
-
     puts "Done!."
+
   else
     puts "Please make sure that both source and target are valid folders."
   end
